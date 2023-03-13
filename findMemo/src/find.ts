3ac14018d2e75aef70b1367b84aed3ee7e88131b
charu-gera <charu.gera@valuelabs.com>
2023-03-13 22:14:09 +0530

    findmemo asset update

--- findMemo/src/find.ts
+++ findMemo/src/find.ts
@@ -2,37 +2,35 @@ import { Asset, Server, ServerApi } from "stellar-sdk";
 
 const server: Server = new Server("https://horizon-testnet.stellar.org");
 export type Record = ServerApi.OperationRecord;
-export type TxRecord = ServerApi.TransactionRecord;
-//const record_date: string = "2023-03-02T07:09:04Z";
 
 const searchCriteria = async (
   record: Record,
-  issuer: string,
+  asset: Asset,
   kmemo: string,
   record_date: string
 ) => {
   if (
-    record.source_account === issuer &&
+    record.source_account === asset.issuer &&
     record.type === "payment" &&
-    record.asset_code === "TCOIN" &&
+    record.asset_code === asset.code &&
     new Date(record.created_at) >= new Date(record_date)
   ) {
     const tx = await record.transaction();
     if (tx.memo === kmemo) {
-      console.log(" found ", record.transaction_hash);
+      console.log(" found memo", record.transaction_hash);
       return true;
     }
   }
 };
 
 export const findMemo = async (
-  issuer: string,
+  asset: Asset,
   kmemo: string,
   record_date: string
 ): Promise<Record> => {
   let page = await server
     .operations()
-    .forAccount(issuer)
+    .forAccount(asset.issuer)
     .order("desc")
     .limit(30)
     .call();
@@ -42,18 +40,16 @@ export const findMemo = async (
   while (page.records.length && !found) {
     filtered = [];
     filtered = page.records.filter(async (record) => {
-      const f = await searchCriteria(record, issuer, kmemo, record_date);
+      const f = await searchCriteria(record, asset, kmemo, record_date);
       if (f) {
         found = f;
         memo_record = record;
         return found;
-        //  return record;
       }
     });
 
     if (!found) page = await page.next();
     console.log("found - ", found);
   }
-  //memo_record = filtered[0];
   return memo_record;
 };
